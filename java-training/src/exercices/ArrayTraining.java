187ed752a42dd668085925f2a75671e97dc30092
Adrien Souquere <adriensouquere@gmail.com>
2019-10-02 14:49:29 +0200

    trainig 03

---
+++ java-training/src/exercices/ArrayTraining.java
@@ -0,0 +1,181 @@
+public class ArrayTraining {
+
+    /**
+     * @param n size of the array, eg: 3
+     * @return an empty int array of size n, eg: {0, 0, 0}
+     */
+    public int[] emptyIntArray(int n) {
+            int array[] = new int[n];
+        return array;
+    }
+
+    /**
+     * @param n size of the array, eg: 2
+     * @return an empty String array of size n, eg: {null, null}
+     */
+    public String[] emptyStringArray(int n) {
+            String array[] = new String[n];
+        return array;
+    }
+
+    /**
+     * @param a first value, eg: 3
+     * @param b second value, eg: 2
+     * @param c third value, eg: 1
+     * @return an int array with a, b and c values, eg: {3, 2, 1}
+     */
+    public int[] intArray(int a, int b, int c) {
+            int Array[] = {a, b, c};
+        return Array;
+    }
+
+    /**
+     * @param a first value, eg: "test"
+     * @param b second value, eg: "sample"
+     * @param c third value, eg: "value"
+     * @return an int array with a, b and c values,
+     * eg: {"test", sample", "value"}
+     */
+    public String[] stringArray(String a, String b, String c) {
+            String Array[] = {a, b, c};
+        return Array;
+    }
+
+    /**
+     * @param array, eg: {1, 2, 3}
+     * @return length of the array, eg: 1
+     */
+    public int length(int[] array) {
+            int arraylength = array.length;
+        return arraylength;
+    }
+
+    /**
+     * @param array, eg: {3, 2, 1}
+     * @return first value of the array, eg: 3
+     */
+    public int firstValue(int[] array) {
+            int firstValue = array[0];
+        return firstValue;
+    }
+
+    /**
+     * @param array, eg: {3, 2, 1}
+     * @return last value of the array, eg: 1
+     */
+    public int lastValue(int[] array) {
+            int lastValue = array[array.length-1];
+        return lastValue;
+    }
+
+    /**
+     * @param array,    eg: {3, 2, 1}
+     * @param position, eg: 1
+     * @return value of the array at position, eg: 2
+     */
+    public int valueAtPosition(int[] array, int position) {
+            int posVal =array[position];
+        return posVal;
+    }
+
+    /**
+     * @param array,    eg: {3, 8, 5}
+     * @param value,    eg: 4
+     * @param position, eg: 1
+     * @return the array with the value replaced at position, eg: {3, 4, 5}
+     */
+    public int[] replace(int[] array, int value, int position) {
+            array[position] = value;
+        return array;
+    }
+
+    /**
+     * @param array, eg: {1, 2, 3}
+     * @return sum of values of the array, eg: 6
+     */
+    public int sum(int[] array) {
+            int arraySum = 0;
+            for (int arraylength = 0; arraylength < array.length; arraylength++){
+                arraySum += array[arraylength];
+            }
+        return arraySum;
+    }
+
+    /**
+     * @param array,  eg: {1, 2, 3}
+     * @param search, eg: 2
+     * @return if array contains searched value, eg: true
+     */
+    public boolean contains(int[] array, int search) {
+            boolean isIn = false;
+            for (int i = 0; i < array.length; i++){
+                if (array[i] == search){
+                    isIn = true;
+                }
+            }
+        return isIn;
+    }
+
+    /**
+     * @param array, eg: {"sample", "value"}
+     * @return array where all words are capitalized, eg: {"Sample", "Value"}
+     */
+    public String[] capitalize(String[] array) {
+        for(int arrayLength = 0; arrayLength < array.length; arrayLength++){
+            String capitalizedWorld = array[arrayLength].substring(0,1).toUpperCase() + array[arrayLength].substring(1);
+            array[arrayLength] = capitalizedWorld;
+        }
+        return array;
+    }
+
+    /**
+     * @param first,  eg: {1, 2, 3}
+     * @param second, eg: {3, 2, 1}
+     * @return if values in first and second arrays, in the same position,
+     * are equals, eg: false
+     */
+    public boolean equals(int[] first, int[] second) {
+        int matchCount = 0;
+		boolean same = false;		
+		for (int arrayLength = 0 ; arrayLength < first.length ; arrayLength++){
+			if (first[arrayLength] == second[arrayLength]){
+				matchCount += 1;
+			}
+		}
+			if (matchCount == first.length){
+				same = true;
+			}
+        return same;
+    }
+
+    /**
+     * @param array, eg: {1, 2, 3}
+     * @return array with reversed values, eg: {3, 2, 1}
+     */
+    public int[] reverse(int[] array) {
+        int[] reversedArray = new int [array.length];
+		int reversedIndx = array.length - 1;
+		for (int i = 0 ; i < array.length ; i++){	
+			reversedArray[i] = array[reversedIndx];
+			reversedIndx--;
+		}
+        return reversedArray;
+    }
+
+    /**
+     * @param first,  eg: {1, 2, 3}
+     * @param second, eg: {4, 5}
+     * @return concatenates first and second values in one array,
+     * eg: {1, 2, 3, 4, 5}
+     */
+    public int[] concat(int[] first, int[] second) {
+        int[] mergedArrays = new int[first.length + second.length];      
+        for(int firstIndex = 0; firstIndex < first.length; firstIndex++) { 
+           mergedArrays[firstIndex] = first[firstIndex];         
+        } 
+        for(int secondIndex = 0 ; secondIndex < second.length ; secondIndex++){ 
+           mergedArrays[secondIndex + first.length] = second[secondIndex];
+        }   
+        return mergedArrays;
+    }
+}
---
+++ java-training/src/exercices/MatrixTraining.java
@@ -0,0 +1,152 @@
+public class MatrixTraining {
+
+    /**
+     * @param lines   height of the matrix, eg: 3
+     * @param columns width of the matrix, eg: 2
+     * @return an empty int matrix of lines and columns size,
+     * eg: {{0, 0}, {0, 0}, {0, 0}}
+     */
+    public int[][] emptyIntMatrix(int lines, int columns) {
+        int matrix[][] = new int [lines][columns];
+        return matrix;
+    }
+
+    /**
+     * @param lines   height of the matrix, eg: 2
+     * @param columns width of the matrix, eg: 3
+     * @return an empty string matrix of lines and columns size,
+     * eg: {{null, null, null}, {null, null, null}}
+     */
+    public String[][] emptyStringMatrix(int lines, int columns) {
+        String matrix[][] = new String [lines][columns];
+        return matrix;
+    }
+
+    /**
+     * @param a first array
+     * @param b second array
+     * @param c third array
+     * @return an int matrix with a, b and c arrays
+     */
+    public int[][] intMatrix(int[] a, int[] b, int[] c) {
+        int Matrix [][] = {a, b, c};
+        return Matrix;
+    }
+
+    /**
+     * @param a first array
+     * @param b second array
+     * @param c third array
+     * @return a String matrix with a, b and c arrays
+     */
+    public String[][] stringMatrix(String[] a, String[] b, String[] c) {
+        String matrix [][] = {a, b, c};
+        return matrix;
+    }
+
+    /**
+     * @param matrix, eg: {{3, 4}, {6, 7}, {5, 8}}
+     * @return number of matrix lines, eg: 3
+     */
+    public int height(int[][] matrix) {
+
+        return matrix.length;
+    }
+
+    /**
+     * @param matrix, eg: {{3, 4}, {6, 7}, {5, 8}}
+     * @return number of matrix columns, eg: 2
+     */
+    public int width(int[][] matrix) {
+
+        return matrix[0].length;
+    }
+
+    /**
+     * @param matrix, eg: {{3, 4, 5}, {6, 7, 8}}
+     * @param line,   eg: 1
+     * @param column, eg: 0
+     * @return value in matrix at line and column, eg: 6
+     */
+    public int valueAtPosition(int[][] matrix, int line, int column) {
+
+        return matrix [line][column];
+    }
+
+    /**
+     * @param matrix, eg: {{1, 2, 3}, {8, 5, 6}}
+     * @param value,  eg: 4
+     * @param line,   eg: 1
+     * @param column, eg: 0
+     * @return the matrix with the value replaced at line and column,
+     * eg: {{1, 2, 3}, {4, 5, 6}}
+     */
+    public int[][] replace(int[][] matrix, int value, int line, int column) {
+        matrix [line][column] = value;
+        return matrix;
+    }
+
+    /**
+     * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
+     * @return sum of matrix values, eg: 21
+     */
+    public int sum(int[][] matrix) {
+        int sum = 0;
+		for (int line = 0 ; line < matrix.length ; line++) {
+			for (int column = 0 ; column < matrix[0].length ; column++) {
+				sum = sum + matrix[line][column];
+			}
+		}
+        return sum;
+    }
+
+    /**
+     * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
+     * @param search, eg: 5
+     * @return if matrix contains searched value, eg: true
+     */
+    public boolean contains(int[][] matrix, int search) {
+        boolean isIn = false;		
+		for (int line = 0 ; line < matrix.length ; line++) {
+			for (int column = 0 ; column < matrix[0].length ; column++) { 
+				if (matrix[line][column] == search){
+					 isIn = true;
+				}
+			}
+		}
+        return isIn;
+    }
+
+    /**
+     * @param matrix, eg: {{1, 2, 3}, {4, 5, 6}}
+     * @return how many even numbers are in matrix, eg: 3
+     */
+    public int countEvens(int[][] matrix) {
+        int evenNbrs = 0;			
+		for (int line = 0 ; line < matrix.length ; line++) {
+			for (int column = 0 ; column < matrix[0].length ; column++) {
+				if (matrix[line][column] % 2  == 0) {
+					evenNbrs += 1;
+				}
+			}
+		}
+        return evenNbrs;
+    }
+
+    /**
+     * @param matrix, eg: {{'d', 'b', 'a'}, {'a', 'd', 'a'}}
+     * @param search, eg: 'a'
+     * @return the number of character occurrences in matrix, eg: 3
+     */
+    public int occurrences(char[][] matrix, char search) {
+        int matchCount = 0;			
+		for (int line = 0 ; line < matrix.length ; line++) {
+			for (int column = 0 ; column < matrix[0].length ; column++) {
+				if (matrix[line][column] == search) {
+					matchCount += 1;
+				}
+			}
+		}
+        return matchCount;
+    }
+}
